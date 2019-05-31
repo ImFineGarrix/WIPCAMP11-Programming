2974bfed2f9ce831921c11268be2bfaca7f47185
ImFineGarrix <50834482+ImFineGarrix@users.noreply.github.com>
2019-05-31 08:44:01 +0700

    Add project

---
+++ WIP-XI/src/IV/Operators/_2_Relational.java
@@ -0,0 +1,27 @@
+/*
+ * WIP XI Computer Programing 
+ */
+
+package IV.Operators;
+
+/**
+ *
+ * @author sittiwatlcp
+ */
+public class _2_Relational {
+    public static void main(String[] args) {
+        int x = 6;
+        
+        System.out.println("== : " + (x == 9));
+        
+        System.out.println("!= : " + (x != 16));
+        
+        System.out.println("> : " + (x > 8));
+        
+        System.out.println("< : " + (x < 1));
+        
+        System.out.println(">= : " + (x >= 3));
+        
+        System.out.println("<= : " + (x <= 4));
+    }
+}
